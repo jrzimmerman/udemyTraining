7b3155b2dda2e8d1ac72bad1dfeb21e623da41be
Justin Zimmerman <zimmy130@gmail.com>
2016-08-02 21:29:57 -0400

    multiple package visibility

---
+++ 04_scope/01_package-scope/02_visibility/vis/name.go
@@ -0,0 +1,5 @@
+package vis
+
+// MyName exported to be used in Main
+var MyName = "Justin"
+var yourName = "Other person"
---
+++ 04_scope/01_package-scope/02_visibility/vis/printer.go
@@ -0,0 +1,9 @@
+package vis
+
+import "fmt"
+
+// PrintVar will be exported to package main
+func PrintVar() {
+	fmt.Println(MyName)
+	fmt.Println(yourName)
+}
